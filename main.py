da537aa9fd567b93b0298d2aba78abc1339d2e71
Panpan 3 <theoo3103@gmail.com>
2017-04-04 17:43:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,6 +42,27 @@ def bruit_L(valeur):
 				pxl.append(j)
 		p=(pxl[0], pxl[1], pxl[2])
 		data[i]=p
+def bruit_C(valeur):
+	global data
+	for i in range(len(data)):
+		p=data[i]
+		r=randint(0,2)
+		pxl=[0]*3
+		for k in range(0,3):
+			if k==r:
+				j=int(p[k]+255*(int(valeur)/100))
+				if j>255:
+					pxl[k]=255
+				else:
+					pxl[k]=j
+			else:
+				j=p[k]-255*(int(valeur)/100))
+				if j<0:
+					pxl[k]=0
+				else:
+					pxl[k]=j
+		p=(pxl[0], pxl[1], pxl[2])
+		data[i]=p
 valeur=input("?")
 bruit_L(valeur)
 # print(data)
